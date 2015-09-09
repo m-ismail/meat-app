9beb0e1752ec4ec049b9de846894637eb0481849
MIsmail <m.ismail@danatdigital.com>
2015-09-09 11:49:44 +0400

    -- fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,26 +4,29 @@ use App\AnimalType;
 use App\CuttingMethod;
 use App\DistributionCenter;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
 
 
-	/**
-	 * Show the application dashboard to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$types = AnimalType::all();
-		$methods = CuttingMethod::all();
-		$centers = DistributionCenter::all();
+    /**
+     * Show the application dashboard to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $types = AnimalType::all();
+        $methods = CuttingMethod::all();
+        $centers = DistributionCenter::all();
 
-		return response()->json(['data' => [
-			'types' => $types,
-			'methods' => $methods,
-			'centers' => $centers,],
-			'code' => 200
-		], 200);
-	}
+        return response()->json([
+            'data' => [
+                'types' => $types,
+                'methods' => $methods,
+                'centers' => $centers,
+            ],
+            'code' => 200
+        ], 200);
+    }
 
 }
